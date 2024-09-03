ad6c89eed857fe1de2ca1991ebb831a74e95ed2b
Stephanie Souza <stephaniebioufmg@gmail.com>
2024-09-02 21:29:16 -0300

    insere backend

--- FocinhosCarinhosos/BackEnd/src/config/dbConect.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-async function conectaDatabase(){
-    mongoose.connect(process.env.DB_CONNECTION_STRING);
-    return mongoose.connection;
-};
-
-export default conectaDatabase;
--- FocinhosCarinhosos/BackEnd/src/routes/user.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { login } from '../controllers/userController.js';
-
-const routes = express.Router();
-
-routes.post('/login', login);
-
-export default routes;
