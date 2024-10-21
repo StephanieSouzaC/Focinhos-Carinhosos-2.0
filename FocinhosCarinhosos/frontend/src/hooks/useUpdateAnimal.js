743702ebcaf61a916a541232f8e91b9b1d2e528b
Stephanie Souza <stephaniebioufmg@gmail.com>
2024-10-21 17:14:55 -0300

    remove comentários e codigos não utilizados

--- FocinhosCarinhosos/frontend/src/hooks/useUpdateAnimal.js
+++ FocinhosCarinhosos/frontend/src/hooks/useUpdateAnimal.js
@@ -1,33 +1,3 @@
-// import { useState } from "react";
-// import api from '../configApi/api.js';
-
-// const useUpdateAnimal = () => {
-//   const [loading, setLoading] = useState(false);
-//   const [error, setError] = useState(null);
-//   const [success, setSuccess] = useState(false);
-
-//   const updateAnimal = async (animalId, animalData) => {
-//     setLoading(true);
-//     setError(null);
-//     setSuccess(false);
-
-//     try {
-//       const response = await api.put(`/Animais/${animalId}`, animalData); 
-//       if (response.status === 200) {
-//         setSuccess(true);
-//       }
-//     } catch (err) {
-//       setError(err.response ? err.response.data.message : "Erro ao atualizar o animal");
-//     } finally {
-//       setLoading(false);
-//     }
-//   };
-
-//   return { updateAnimal, loading, error, success };
-// };
-
-// export default useUpdateAnimal;
-
 import { useState } from "react";
 import api from "../configApi/api"; 
 
